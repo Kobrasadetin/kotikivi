3fbee37354e4935e80de88d2290cbf3367275b4e
Mikko Karjanmaa <mikko.karjanmaa@arter.fi>
2019-01-26 14:10:10 +0200

    add ground visualisation

---
+++ Assets/Scripts/Visual/PrefabChangeEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using Graph;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class PrefabChangeEvaluator
+{
+
+    public static readonly float sideLength = (float)System.Math.Cos(System.Math.PI / 6.0);
+
+    public abstract void UpdatePrefab(Visual.VisualNode visualNode, GraphNode node);
+
+    public Vector2 calculatePosition(GraphNode node)
+    {
+        float oddOffset = (node.Coordinate.y % 2) * (sideLength);
+        float xposition = node.Coordinate.x * sideLength *2  + oddOffset;
+        float yposition = node.Coordinate.y * 1.5f;
+        return new Vector2(xposition, yposition);
+    }
+}
---
+++ Assets/Scripts/Visual/VisualRoot.cs
@@ -0,0 +1,29 @@
+﻿using Graph;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Visual
+{
+    public class VisualRoot : MonoBehaviour
+    {
+        Graph.Graph graph;
+        // Start is called before the first frame update
+        void Start()
+        {
+            graph = Graph.GraphSingleton.Instance.Graph;
+            graph.Nodes.ForEach(node =>
+               {
+                   VisualNodeFactory.CreateTile(this.transform, node);
+               }
+            );
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
